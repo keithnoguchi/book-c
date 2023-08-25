ce7b4a6ea9336f93c2b7a20c6fbe8cc5ee8845fa
Keith Noguchi <keith@noguchi.us>
2023-08-25 16:40:31 -0700

    ch01: Fahrenheit to Celsius table

--- ch01/main.c
+++ ch01/main.c
@@ -1,7 +1,21 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
+/* print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300 */
 int main()
 {
-	printf("hello world\n");
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
 }
