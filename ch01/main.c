cecdf46ddc0caf2f1d17acf8bc0ead77bcf7ad46
Keith Noguchi <keith@noguchi.us>
2023-08-25 17:26:06 -0700

    use integer for the fahrenheit, which is faster than float

--- ch01/main.c
+++ ch01/main.c
@@ -5,11 +5,10 @@
  * for fahr = 0, 20, ..., 300 */
 int main()
 {
-	float fahr, celsius;
+	int fahr;
 
-	for (fahr = 0; fahr < 300; fahr += 20) {
-		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-	}
+	for (fahr = 0; fahr < 300; fahr += 20)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
+
 	return 0;
 }
