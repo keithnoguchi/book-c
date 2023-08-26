f94c5e21c2a3a2f714c96ca1a16d5ee913879359
Keith Noguchi <keith@noguchi.us>
2023-08-25 17:51:54 -0700

    ch01: getchar() and putchar()

--- ch01/main.c
+++ ch01/main.c
@@ -1,14 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300 */
 int main()
 {
-	int fahr;
+	int c;
 
-	for (fahr = 300; fahr >= 0; fahr -= 20)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
-
-	return 0;
+	c = getchar();
+	while (c != EOF) {
+		putchar(c);
+		c = getchar();
+	}
 }
