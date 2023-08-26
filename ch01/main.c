3c26de5f3411337fef6eae02947f004be90ae262
Keith Noguchi <keith@noguchi.us>
2023-08-25 17:19:56 -0700

    ch01: use for loop

--- ch01/main.c
+++ ch01/main.c
@@ -6,16 +6,10 @@
 int main()
 {
 	float fahr, celsius;
-	int lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-	while ((int)fahr <= upper) {
+	for (fahr = 0; fahr < 300; fahr += 20) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
 	}
+	return 0;
 }
