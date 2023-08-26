ef2c7cb0f2e6a9ec229792bf66700be272400c77
Keith Noguchi <keith@noguchi.us>
2023-08-25 17:09:26 -0700

    use float

--- ch01/main.c
+++ ch01/main.c
@@ -5,7 +5,7 @@
  * for fahr = 0, 20, ..., 300 */
 int main()
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;
@@ -13,9 +13,9 @@ int main()
 	step = 20;
 
 	fahr = lower;
-	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%3d %6d\n", fahr, celsius);
+	while ((int)fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
