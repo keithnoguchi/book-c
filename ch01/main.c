1f66160c69caadf0d4743155d3c0f1a904c21a84
Keith Noguchi <keith@noguchi.us>
2023-08-26 17:24:54 -0700

    ch01: counts digits, white spaces, and others

--- ch01/main.c
+++ ch01/main.c
@@ -1,27 +1,25 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
+int main() {
+	int c, i, nwhite, nother;
+	int ndigit[10];
 
-int main()
-{
-	int c, nl, nw, nc, state;
+	nwhite = nother = 0;
+	for (i = 0; i < 10; i++)
+		ndigit[i] = 0;
 
-	state = OUT;
-	nl = nw = nc = 0;
+	while ((c = getchar()) != EOF)
+		if (c >= '0' && c <= '9')
+			++ndigit[c - '0'];
+		else if (c == ' ' || c == '\t' || c == '\n')
+			++nwhite;
+		else
+			++nother;
 
-	while ((c = getchar()) != EOF) {
-		++nc;
-
-		if (c == '\n')
-			++nl;
-		if (c == ' ' || c == '\t' || c == '\n')
-			state = OUT;
-		else if (state == OUT) {
-			++nw;
-			state = IN;
-		}
-	}
-	printf("%d %d %d\n", nl, nw, nc);
+	printf("digits = ");
+	for (i = 0; i < 10; i++)
+		printf("%d, ", ndigit[i]);
+	printf(", white space = %d, other = %d\n",
+	       nwhite, nother);
 }
