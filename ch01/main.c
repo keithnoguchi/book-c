645d4f1bf307de26ff46a3e2e8b9162b9b1cdfbb
Keith Noguchi <keith@noguchi.us>
2023-08-26 17:03:12 -0700

    ch01: A simple wc(1)

--- ch01/main.c
+++ ch01/main.c
@@ -1,10 +1,27 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main()
 {
-	int c;
+	int c, nl, nw, nc, state;
 
-	while ((c = getchar()) != EOF)
-		putchar(c);
+	state = OUT;
+	nl = nw = nc = 0;
+
+	while ((c = getchar()) != EOF) {
+		++nc;
+
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\t' || c == '\n')
+			state = OUT;
+		else if (state == OUT) {
+			++nw;
+			state = IN;
+		}
+	}
+	printf("%d %d %d\n", nl, nw, nc);
 }
