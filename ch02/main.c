640549c54fb808507ee6e4ce6eea74631ede83ec
Keith Noguchi <keith@noguchi.us>
2023-08-30 06:49:07 -0700

    ch02: the ternary operator example

--- ch02/main.c
+++ ch02/main.c
@@ -1,22 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
-static int bitcount(unsigned x)
-{
-	int b;
-
-	for (b = 0; x != 0; x >>= 1)
-		if (x & 01)
-			b++;
-
-	return b;
-}
-
 int main()
 {
-	printf("0x%x = %d\n", ~0, bitcount(~0)); // 32 or 64
-						 // depending on
-						 // the unsigned
-						 // type, e.g
-						 // int or long.
+	char a[64];
+	int n, i;
+
+	n = sizeof(a) / sizeof(char);
+	for (i = 0; i < n; i++)
+		a[i] = i + 1;
+
+	/* the ternary operator example */
+	for (i = 0; i < n; i++)
+		printf("%3d%c", a[i],
+		       (i % 7 == 6 || i == n - 1) ? '\n' : ' ');
 }
