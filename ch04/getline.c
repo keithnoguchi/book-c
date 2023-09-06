032dc7269b0777de8d24b7e072fb73f9a6035e69
Keith Noguchi <keith@noguchi.us>
2023-09-06 07:08:31 -0700

    ch04: fix the getline on the short line handling

--- ch04/getline.c
+++ ch04/getline.c
@@ -3,13 +3,14 @@
 
 int getline2(char s[], int limit)
 {
-	int c, i;
+	int i, c = 0;
 
-	for (i = 0; (c = getchar()) != EOF && i < limit; i++) {
+	for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++)
 		s[i] = c;
-		if (c == '\n')
-			break;
+	if (c == '\n') {
+		s[i] = c;
+		i++;
 	}
-
+	s[i] = '\0';
 	return i;
 }
