97d7ffcdfec945e15569c9fc337d016509b0dc4c
Keith Noguchi <keith@noguchi.us>
2023-09-07 06:23:31 -0700

    ch04: atof(3)

--- ch04/atof.c
+++ ch04/atof.c
@@ -1,5 +1,23 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+#include <ctype.h>
+
 double atof(const char s[])
 {
-	return -1.0;
+	double val, power;
+	int i, sign;
+
+	for (i = 0; isblank(s[i]); i++)
+		;
+	sign = s[i] == '-' ? -1 : 1;
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = val * 10.0 + (s[i] - '0');
+	if (s[i] == '.')
+		i++;
+	for (power = 1.0; isdigit(s[i]); i++) {
+		val = val * 10.0 + (s[i] - '0');
+		power *= 10.0;
+	}
+	return sign * val / power;
 }
