5b0d26e422951066922ce663b74792749b93f7ff
Keith Noguchi <keith@noguchi.us>
2023-09-09 06:49:51 -0700

    ch04: Add push() and pop() functions
    As well as how to utilize when it
    calculates the equation.

--- ch04/main.c
+++ ch04/main.c
@@ -6,8 +6,12 @@
 
 extern int getop(void);
 
+static void push(double);
+static double pop(void);
+
 int main(void)
 {
+	double op2;
 	int type;
 	char s[MAXOP];
 
@@ -16,19 +20,49 @@ int main(void)
 		case NUMBER:
 			break;
 		case '+':
+			push(pop() + pop());
 			break;
 		case '*':
+			push(pop() * pop());
 			break;
 		case '-':
+			op2 = pop();
+			push(pop() - op2);
 			break;
 		case '/':
+			op2 = pop();
+			push(pop() / op2);
 			break;
 		case '\n':
+			printf("\t%g\n", pop());
 			break;
 		default:
-			printf("error: unknown command %s\n", s);
+			fprintf(stderr, "error: unknown command %s\n", s);
 			break;
 		}
 	}
 	return 0;
+}
+
+#define MAX 12
+
+static double stack[MAX];
+static int sp;
+
+static void push(double f)
+{
+	if (sp < MAX)
+		stack[sp++] = f;
+	else
+		fprintf(stderr, "error: full of stack\n");
+}
+
+static double pop(void)
+{
+	if (sp > 0)
+		return stack[--sp];
+	else {
+		fprintf(stderr, "error: empty stack\n");
+		return 0.0;
+	}
 }
