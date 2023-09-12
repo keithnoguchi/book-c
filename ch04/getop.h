4c2bd9a646a94c5950028a42009f85283515be06
Keith Noguchi <keith@noguchi.us>
2023-09-12 12:11:51 -0700

    ch04: A simple quick sort

--- ch04/getop.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _GETOP_H
-#define _GETOP_H
-
-#define NUMBER '\0'
-
-extern int getop(char [], int);
-
-#endif /* _GETOP_H */
--- ch04/main.c
+++ ch04/main.c
@@ -53,25 +53,30 @@ void swap(int v[], int i, int j)
 	v[j] = temp;
 }
 
-#define MAX 12
+static void swap(int [], int, int);
 
-static double stack[MAX];
-static int sp;
-
-static void push(double f)
+static void quicksort(int v[], int left, int right)
 {
-	if (sp < MAX)
-		stack[sp++] = f;
-	else
-		fprintf(stderr, "error: full of stack\n");
+	int i, last;
+
+	if (left >= right)
+		return;
+
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+		if (v[i] < v[left])
+			swap(v, ++last, i);
+	swap(v, left, last);
+	quicksort(v, left, last - 1);
+	quicksort(v, last  + 1, right);
 }
 
-static double pop(void)
+void swap(int v[], int i, int j)
 {
-	if (sp > 0)
-		return stack[--sp];
-	else {
-		fprintf(stderr, "error: empty stack\n");
-		return 0.0;
-	}
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
 }
