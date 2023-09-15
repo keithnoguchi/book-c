f89436db709b82aa6752afe1e0527bc897780c68
Keith Noguchi <keith@noguchi.us>
2023-09-15 14:35:04 -0700

    ch05: int getint(int *)

--- ch05/main.c
+++ ch05/main.c
@@ -1,8 +1,61 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
-	printf("Chapter 5: Pointers and Arrays\n");
-	return 0;
+	int ret, i, getint(int *);
+
+	while ((ret = getint(&i)) != EOF) {
+		if (ret == 0) {
+			fprintf(stderr, "not a number\n");
+			break;
+		}
+		printf("\t%d = getint(%d)\n", ret, i);
+	}
+	return !ret;
+}
+
+int getint(int *np)
+{
+	int getch(void);
+	void ungetch(int);
+	int c, sign;
+
+	while (isspace(c = getch()))
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+		return 0;
+	}
+	sign = c == '-' ? -1 : 1;
+	if (c == '+' || c == '-')
+		c = getch();
+	for (*np = 0; isdigit(c); c = getch())
+		*np = *np * 10 + (c - '0');
+	*np *= sign;
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
+
+#define SIZE 10
+
+static int stack[SIZE];
+static int sp;
+
+int getch(void)
+{
+	if (sp > 0)
+		return stack[--sp];
+	else
+		return getchar();
+}
+
+void ungetch(int c)
+{
+	if (sp >= SIZE)
+		fprintf(stderr, "ungetch: stack is full\n");
+	else
+		stack[sp++] = c;
 }
