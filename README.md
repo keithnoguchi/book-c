e25fce9d26c686cc8c6489eb6798689607d060ca
Keith Noguchi <keith@noguchi.us>
2023-09-15 20:43:29 -0700

    ch05: Name of the Month
    5.8 Initialization of Pointer Arrays

--- README.md
+++ README.md
@@ -9,4 +9,4 @@
 - Chapter 4: Functions and Program Structures
   - [A simple quick sort](ch04/main.c)
 - Chapter 5: Pointers and Arrays
-  - [Day of the Year](ch05/main.c)
+  - [Name of the Month](ch05/main.c)
--- ch05/main.c
+++ ch05/main.c
@@ -1,55 +1,24 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
-static char daytab[2][13] = {
-	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-};
-
-static int day_of_year(int, int, int);
-static void month_day(int, int, int *, int *);
+static char *month_name(int);
 
 int main(void)
 {
-	int year, month, day;
-	int ret, *rmonth, *rday;
+	int n;
 
-	year = 2023;
-	month = 9;
-	day = 15;
-
-	ret = day_of_year(year, month, day);
-	printf("%04d/%02d/%02d is %d days of the year.\n",
-	       year, month, day, ret);
-
-	rmonth = &month;
-	rday = &day;
-	month_day(year, ret, rmonth, rday);
-	printf("%d days of year %04d is %04d/%02d/%02d\n",
-	       ret, year, year, *rmonth, *rday);
+	for (n = 0; n < 20; n++)
+		printf("%02d: %s\n", n, month_name(n));
 
 	return 0;
 }
 
-static int day_of_year(int year, int month, int day)
+static char *month_name(int n)
 {
-	int i, leap;
-
-	leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-	for (i = 1; i < month; i++)
-		day += daytab[leap][i];
-
-	return day;
-}
-
-static void month_day(int year, int day_of_year, int *month, int *day)
-{
-	int i, leap;
-
-	leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-	for (i = 1; day_of_year > daytab[leap][i]; i++)
-		day_of_year -= daytab[leap][i];
-
-	*month = i;
-	*day = day_of_year;
+	static char *name[] = {
+		"Illegal month", "January", "February", "March",
+		"April", "May", "June", "July", "August", "September",
+		"October", "November", "December",
+	};
+	return (n < 1 || n > 12) ? name[0] : name[n];
 }
