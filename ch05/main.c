a991c2d82f60813c91085f5a6b3838a2d1bbad37
Keith Noguchi <keith@noguchi.us>
2023-09-16 07:36:35 -0700

    ch05: sort(1) with -n command line option

--- ch05/main.c
+++ ch05/main.c
@@ -1,60 +1,106 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
-#include <string.h>
 
-static int getaline(char *, int);
+#define MAXLINE 4
+static char *lines[MAXLINE];
+
+static int readlines(char *[], int);
+static void writelines(char *[], int);
 
 int main(int argc, char *argv[])
 {
-#define MAXLINE 80
 	const char *progname = argv[0];
-	char line[MAXLINE];
-	long lineno = 0;
-	int c, except = 0, number = 0, found = 0;
+	int c, n, numeric = 0, ret = 0;
 
 	while (--argc > 0 && (*++argv)[0] == '-')
 		while ((c = *++argv[0]))
 			switch (c) {
-			case 'x':
-				except = 1;
-				break;
 			case 'n':
-				number = 1;
+				numeric = 1;
 				break;
 			default:
-				fprintf(stderr, "illegal option %c\n", c);
+				fprintf(stderr, "invalid option: %c\n", c);
 				argc = 0;
-				found = -1;
+				ret = 1;
 				break;
 			}
-
 	if (argc != 1)
-		printf("usage: %s -x -n pattern\n", progname);
+		printf("usage: %s -n pattern\n", progname);
 	else
-		while (getaline(line, sizeof(line)) > 0) {
-			lineno++;
-			if ((strstr(line, *argv) != NULL) != except) {
-				found++;
-				if (number)
-					printf("%ld: ", lineno);
-				printf("%s", line);
-			}
-		}
-	return found;
+		while ((n = readlines(lines, MAXLINE)) > 0)
+			writelines(lines, n);
+
+	printf("numeric=%d\n", numeric);
+	return ret;
 }
 
-static int getaline(char *line, int limit)
+static int readlines(char *lines[], int nr)
 {
-	char *p;
-	int c;
+	char *alloc(int), *strcpy(char *, char *);
+#define LINESIZE 80
+	char line[LINESIZE], *p, *limit;
+	int n, c;
 
-	for (p = line; (c = getchar()) != EOF && p - line < limit - 1; p++) {
-		*p = c;
-		if (c == '\n')
+	limit = line + LINESIZE - 1;
+	for (n = 0; n < nr; n++) {
+		for (p = line; p < limit && (c = getchar()) != EOF; p++) {
+			*p = c;
+			if (c == '\n')
+				break;
+		}
+		if (c == EOF)
 			break;
+		else if (c == '\n')
+			p++;
+		*p++ = '\0';
+		lines[n] = alloc(p - line);
+		strcpy(lines[n], line);
 	}
-	if (c == '\n')
-		++p;
-	*p = '\0';
-	return p - line;
+	return n;
+}
+
+static void writelines(char *lines[], int nr)
+{
+	void afree(char *);
+	int n;
+
+	for (n = 0; n < nr; n++)
+		printf("%s", lines[n]);
+
+	/* free lines in reverse order */
+	for (n = nr - 1; n >= 0; n--) {
+		afree(lines[n]);
+		lines[n] = NULL;
+	}
+}
+
+char *strcpy(char *s, char *t)
+{
+	char *start = s;
+	while ((*s++ = *t++))
+		;
+	return start;
+}
+
+#define BUFSIZE 1000
+static char allocbuf[BUFSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int nr)
+{
+	if (allocp + nr <= allocbuf + BUFSIZE) {
+		allocp += nr;
+		return allocp - nr;
+	} else {
+		fprintf(stderr, "alloc: overflow with nr=%d\n", nr);
+		return NULL;
+	}
+}
+
+void afree(char *p)
+{
+	if (p >= allocbuf && p < allocp)
+		allocp = p;
+	else
+		fprintf(stderr, "free: invalid pointer p=%p\n", p);
 }
