06ea91b0db148bef8077df261c571c71b91f79f5
Keith Noguchi <keith@noguchi.us>
2023-09-15 22:08:14 -0700

    ch05: echo(1)

--- README.md
+++ README.md
@@ -9,4 +9,4 @@
 - Chapter 4: Functions and Program Structures
   - [A simple quick sort](ch04/main.c)
 - Chapter 5: Pointers and Arrays
-  - [Name of the Month](ch05/main.c)
+  - [echo(1)](ch05/main.c)
--- ch05/main.c
+++ ch05/main.c
@@ -1,24 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 
-static char *month_name(int);
-
-int main(void)
+int main(int argc, char *argv[])
 {
-	int n;
-
-	for (n = 0; n < 20; n++)
-		printf("%02d: %s\n", n, month_name(n));
+	while (--argc > 0)
+		printf("%s%s", *++argv, argc > 0 ? " " : "");
+	printf("\n");
 
 	return 0;
-}
-
-static char *month_name(int n)
-{
-	static char *name[] = {
-		"Illegal month", "January", "February", "March",
-		"April", "May", "June", "July", "August", "September",
-		"October", "November", "December",
-	};
-	return (n < 1 || n > 12) ? name[0] : name[n];
 }
