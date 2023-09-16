26712d8a31da6629683e71de715dbc104fab10b9
Keith Noguchi <keith@noguchi.us>
2023-09-15 17:02:20 -0700

    ch05: getaline() reads line with null terminated result

--- ch05/main.c
+++ ch05/main.c
@@ -19,15 +19,49 @@ int main(void)
 	return 0;
 }
 
-int readlines(char *lineptr[], int maxline)
+int readlines(char *lineptr[], int maxlines)
 {
-	return -1;
+#define MAXLEN 10
+	char *p, *alloc(int), line[MAXLEN], *strcpy(char *, char *);
+	int len, nlines, getaline(char *, int);
+
+	nlines = 0;
+	while ((len = getaline(line, MAXLEN)) > 0) {
+		printf("%d=getline('%s')\n", len, line);
+		if (nlines >= maxlines || (p = alloc(len)) == NULL)
+			return -1;
+		else {
+			/* drop the newline character */
+			line[len - 1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+	}
+	return nlines;
 }
 
 void writelines(char *lineptr[], int nlines)
 {
 	while (nlines-- > 0)
 		printf("%s\n", *lineptr++);
+}
+
+int getaline(char *line, int nlimit)
+{
+	char *p, *limit;
+	int c = EOF;
+
+	for (p = line, limit = p + nlimit; p < limit - 1 && (c = getchar()) != EOF && c != '\n'; p++)
+		*p = c;
+	if (c == '\n' && p < limit - 2)
+		*p++ = c;
+	*p = '\0';
+	return p - line;
+}
+
+char *alloc(int n)
+{
+	return NULL;
 }
 
 void quicksort(char *v[], int left, int right)
