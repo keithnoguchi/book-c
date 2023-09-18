2250c41daebbd305dfa9bf932fd40fc0d8a9640e
Keith Noguchi <keith@noguchi.us>
2023-09-18 07:20:04 -0700

    ch04: cleanup the rebase error

--- ch04/main.c
+++ ch04/main.c
@@ -7,9 +7,6 @@
 
 static void quicksort(int v[], int left, int right);
 
-static void push(double);
-static double pop(void);
-
 int main(void)
 {
 	int v[50];
@@ -23,34 +20,6 @@ int main(void)
 
 	for (i = 0; i < sizeof(v) / sizeof(int); i++)
 		printf("%2d%c", v[i], i % 10 == 9 ? '\n' : ' ');
-}
-
-static void swap(int [], int, int);
-
-static void quicksort(int v[], int left, int right)
-{
-	int i, last;
-
-	if (left >= right)
-		return;
-
-	swap(v, left, (left + right) / 2);
-	last = left;
-	for (i = left + 1; i <= right; i++)
-		if (v[i] < v[left])
-			swap(v, ++last, i);
-	swap(v, left, last);
-	quicksort(v, left, last - 1);
-	quicksort(v, last  + 1, right);
-}
-
-void swap(int v[], int i, int j)
-{
-	int temp;
-
-	temp = v[i];
-	v[i] = v[j];
-	v[j] = temp;
 }
 
 static void swap(int [], int, int);
