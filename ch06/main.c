17f3061aaba1b452a7d6093c597a403130f711a7
Keith Noguchi <keith@noguchi.us>
2023-09-24 07:29:00 -0700

    ch06: Complete the word counter

--- ch06/main.c
+++ ch06/main.c
@@ -29,9 +29,69 @@ int main(void)
 	return 0;
 }
 
-struct node *add(struct node *root, const char *word)
+struct node *add(struct node *p, const char *word)
 {
-	return NULL;
+	struct node *alloc(void);
+	char *strdup(const char *);
+	int strcmp(const char *, const char *);
+	int cond;
+
+	if (p == NULL) {
+		p = alloc();
+		p->word = strdup(word);
+		p->count = 1;
+		p->left = p->right = NULL;
+	} else if ((cond = strcmp(word, p->word)) == 0)
+		p->count++;
+	else if (cond < 0)
+		p->left = add(p->left, word);
+	else
+		p->right = add(p->right, word);
+	return p;
+}
+
+struct node *alloc(void)
+{
+	return (struct node *)malloc(sizeof(struct node));
+}
+
+char *strdup(const char *s)
+{
+	unsigned long strlen(const char *);
+	char *strcpy(char *, const char *);
+	char *t;
+
+	t = (char *)malloc(strlen(s) + 1);
+	if (t != NULL)
+		strcpy(t, s);
+	return t;
+}
+
+int strcmp(const char *s, const char *t)
+{
+	int i;
+
+	for (i = 0; s[i] == t[i]; i++)
+		if (t[i] == '\0')
+			return 0;
+	return s[i] - t[i];
+}
+
+unsigned long strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	return len;
+}
+
+char *strcpy(char *s, const char *t)
+{
+	while (*t != '\0')
+		*s++ = *t++;
+	*s = '\0';
+	return s;
 }
 
 void print(const struct node *root)
