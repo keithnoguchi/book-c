a364d533362afde480261dfb291eeb532da6fd89
Keith Noguchi <keith@noguchi.us>
2023-09-28 07:09:40 -0700

    switching to toupper through the program name
    It's a bit weak, as it's an exact comparison
    but is a good start with.

--- ch07/main.c
+++ ch07/main.c
@@ -1,13 +1,18 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int c;
+	int (*f)(int) = tolower;
+
+	if (strcmp(argv[0], "./toupper") == 0)
+		f = toupper;
 
 	while ((c = getchar()) != EOF)
-		putchar(tolower(c));
+		putchar((*f)(c));
 
 	return 0;
 }
