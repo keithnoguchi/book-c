9c5f6c3c4294fd62f158809fcd192bc4d97c6394
Keith Noguchi <keith@noguchi.us>
2023-10-04 07:29:41 -0700

    ch07: fgets(3) and fputs(3)

--- ch07/main.c
+++ ch07/main.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
 	void filecopy(FILE *, FILE *);
+	const char *progname = *argv;
 	FILE *fp;
 
 	if (argc == 1)
@@ -11,19 +12,25 @@ int main(int argc, char *argv[])
 	else
 		while (--argc > 0)
 			if ((fp = fopen(*++argv, "r")) == NULL) {
-				fprintf(stderr, "cat: can't open %s\n", *argv);
+				fprintf(stderr, "%s: can't open file %s\n",
+					progname, *argv);
 				return 1;
 			} else {
 				filecopy(fp, stdout);
 				fclose(fp);
 			}
+
+	if (ferror(stdout)) {
+		fprintf(stderr, "%s: error on stdout\n", progname);
+		return 2;
+	}
 	return 0;
 }
 
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(FILE *ip, FILE *op)
 {
 	int c;
 
-	while ((c = getc(ifp)) != EOF)
-		putc(c, ofp);
+	while ((c = getc(ip)) != EOF)
+		putc(c, op);
 }
